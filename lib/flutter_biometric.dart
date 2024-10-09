2a7e61d71c61e52827976644aa1a9ffb8c420ba5
_건유1019 <gunyu1019@gmail.com>
2024-10-09 03:16:06 +0000

    [Feat] Add authenticate and canAuthorization method at flutter biometric class

--- lib/flutter_biometric.dart
+++ lib/flutter_biometric.dart
@@ -1,8 +1,21 @@
 
 import 'flutter_biometric_platform_interface.dart';
+import 'biometric_status.dart';
+import 'options/base_option.dart';
 
 class FlutterBiometric {
-  Future<String?> getPlatformVersion() {
-    return FlutterBiometricPlatform.instance.getPlatformVersion();
-  }
+    Future<String?> getPlatformVersion() {
+        return FlutterBiometricPlatform.instance.getPlatformVersion();
+    }
+    
+    Future<BiometricStatus> canAuthorization() {
+        return FlutterBiometricPlatform.instance.canAuthorization();
+    }
+
+    Future<bool> authenticate(
+        String title,
+        BaseOption option
+    ) {
+        return FlutterBiometricPlatform.instance.authenticate();
+    }
 }
